d35aa14868022cabe3c945e36e9d8e158a4e7cbf
Vimagadum <95463195+Vimagadum@users.noreply.github.com>
2022-01-28 19:20:38 +0530

    [Vinayak] created linked list to add items

---
+++ DataStructure/DataStructure/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DataStructure;
+Console.WriteLine("Data Structure");
+LinkedList customLinkedList = new LinkedList();
+customLinkedList.AddNode(56);
+customLinkedList.AddNode(30);
+customLinkedList.AddNode(76);
+Console.ReadLine();
+
